cb1cf588e4de4e7893ac7f6cab9f4a2efd591613
AntonBrito <tonybrito85@gmail.com>
2017-10-24 12:12:31 -0400

    I eddited all my scripts
    now the back end is link to heroku

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,7 @@
 class Provider < ApplicationRecord
   belongs_to :user
   # belongs_to :appointment
+  has_many :users, dependent: :destroy
   has_many :events, dependent: :destroy
+  has_many :appointments, dependent: :destroy
 end
--- scripts/get-events.sh
+++ scripts/get-events.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-# API="${API_ORIGIN:-https://serene-bastion-62784.herokuapp.com/}"
+API="${API_ORIGIN:-https://wave-suave.herokuapp.com/}"
+# API="${API_ORIGIN:-http://localhost:4741}"
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/events"
 curl "${API}${URL_PATH}" \
